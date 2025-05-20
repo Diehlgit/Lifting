49643e0b8fb51d449ddcaafcabaaca49d525e503
Arthur Diehl Barroso <arthurdiehlbarroso@gmail.com>
2025-05-20 20:37:27 -0300

    Added definitions of what is a Presence Condition.

---
+++ STLC_SuccNat/Lifted_STLC_SuccNat.v
@@ -0,0 +1,59 @@
+Require Import STLC_SuccNat.
+Require Import String List.
+Import ListNotations.
+
+Open Scope string_scope.
+(* A Feature is represented by a string *)
+Definition Feature := string.
+
+(* A Presence Condition is a Boolean expression over features *)
+Inductive PresenceCondition : Type :=
+  | PC_Feature : Feature -> PresenceCondition
+  | PC_And     : PresenceCondition -> PresenceCondition -> PresenceCondition
+  | PC_Or      : PresenceCondition -> PresenceCondition -> PresenceCondition
+  | PC_Not     : PresenceCondition -> PresenceCondition.
+
+Declare Custom Entry pc_syntax.
+
+Notation "x" := x (in custom pc_syntax at level 0, x constr).
+Notation "'FEATURE' f" := (PC_Feature f)
+  (in custom pc_syntax at level 0, f constr).
+Notation "x '/\' y" := (PC_And x y)
+  (in custom pc_syntax at level 40, left associativity).
+Notation "x '\/' y" := (PC_Or x y)
+  (in custom pc_syntax at level 50, left associativity).
+Notation "'~' x" := (PC_Not x)
+  (in custom pc_syntax at level 35, right associativity).
+Notation "'(' x ')'" := x
+  (in custom pc_syntax, x at level 0).
+Notation "'[[' x ']]'" := x
+  (at level 0, x custom pc_syntax at level 99).
+
+(*A Feature Configuration is a list of features *)
+Definition FeatureConfig := list Feature.
+
+(* Function that evaluates a Presence Condition given a Feature Configuration *)
+(* Q: Should I do a relational definition of this function?
+      Since it might be easier to prove somethings *)
+Fixpoint pc_eval (cfg : FeatureConfig) (pc : PresenceCondition) : bool :=
+  match pc with
+  | PC_Feature f => if in_dec String.string_dec f cfg then true else false
+  | PC_And p1 p2 => pc_eval cfg p1 && pc_eval cfg p2
+  | PC_Or  p1 p2 => pc_eval cfg p1 || pc_eval cfg p2
+  | PC_Not p1   => negb (pc_eval cfg p1)
+  end.
+
+Definition pc1 := [[ (FEATURE "A") /\ (FEATURE "B") ]].
+Compute pc_eval ["A"; "B"] pc1.
+
+Definition pc2 := [[ ~ FEATURE "C" ]].
+Compute pc_eval [] pc2.
+
+Definition pc3 := [[ (FEATURE "A") /\ ~ (FEATURE "B") \/ (FEATURE "C") ]].
+Compute pc_eval ["A"; "B"; "C"] pc2.
+
+
+
+
+
+(* Lifted Types *)
