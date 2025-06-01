3c11a900918834c1edfc632c343872e05579a547
Arthur Diehl Barroso <arthurdiehlbarroso@gmail.com>
2025-06-01 18:34:09 -0300

    Added definitions of the lifted language.

--- STLC_SuccNat/Lifted_STLC_SuccNat.v
+++ STLC_SuccNat/Lifted_STLC_SuccNat.v
@@ -116,13 +116,9 @@ Definition lifted_plustwo := [(abs "n" Nat (succ (succ (var "n"))), [[TRUE]])].
 Check lifted_plustwo.
 
 (* It's possible to define this like a function that receives a ty and returns a lifted_ty *)
-(* Definition lifted_ty := list (ty * PresenceCondition). *)
 Definition lifted_tm := list (tm * PresenceCondition).
 
-Definition disjoint (pc1 pc2 : PresenceCondition) : Prop :=
-  forall (cfg : FeatureConfig), ~(pc_eval cfg pc1 = true /\ pc_eval cfg pc2 = true).
-
-Example disjoint1: disjoint [[TRUE]] [[FALSE]].
+(* Example disjoint1: disjoint [[TRUE]] [[FALSE]].
 Proof.
   intros cfg [Hpc1 Hpc2].
   rewrite <- Hpc1 in Hpc2.
@@ -152,21 +148,21 @@ Proof.
     try (inversion Hpc1);
     inversion Hpc2.
 Qed.
-
-Fixpoint disjointness (lftd_tm : lifted_tm) : Prop :=
+ *)
+(* Fixpoint disjointness (lftd_tm : lifted_tm) : Prop :=
   match lftd_tm with
   | [] => True
   | (_, pc) :: rest =>
       (forall t' pc', In (t', pc') rest -> disjoint pc pc') /\ disjointness rest
   end.
-
+ *)
 Definition lifted_x :=
   [
    (const 1, [[FEATURE "A"]]);
    (const 2, [[~ (FEATURE "A") /\ (FEATURE "B")]]);
    (const 3, [[~ (FEATURE "A") /\ ~ (FEATURE "B")]])
   ].
-
+(* 
 Example disjointness_lftd_x: disjointness lifted_x.
 Proof.
   simpl. split.
@@ -189,7 +185,7 @@ Proof.
       * inversion H.
     + split; auto.
       intros. inversion H.
-Qed.
+Qed. *)
 
 Definition lifted_y :=
   [
@@ -198,7 +194,7 @@ Definition lifted_y :=
     (const 3, [[~(FEATURE "A") /\ ~(FEATURE "B")]])
   ].
 
-(*Definetively can make a Ltac to solve this stuff*)
+(* (*Definetively can make a Ltac to solve this stuff*)
 Example disjointness_lftd_y: disjointness lifted_y.
 Proof.
   split.
@@ -238,10 +234,9 @@ Proof.
   split.
   - intros. inversion H.
   - apply I.
-Qed.
+Qed. *)
 
-(* Automatic lifting *)
-Fixpoint lift (t:tm) : lifted_tm :=
+(* Fixpoint lift (t:tm) : lifted_tm :=
   match t with
   | var s => [(var s, [[TRUE]])]
   | STLC_SuccNat.app t1 t2 =>
@@ -253,9 +248,9 @@ Fixpoint lift (t:tm) : lifted_tm :=
     map (fun '(tm, pc) => (succ tm, pc)) (lift t1)
   | abs s T t1 =>
     map (fun '(tm, pc) => (abs s T tm, pc)) (lift t1)
-  end.
+  end. *)
 
-Example lifting_plusone: lift plusone = lifted_plusone.
+(* Example lifting_plusone: lift plusone = lifted_plusone.
 Proof.
   unfold lifted_plusone.
   simpl. reflexivity.
@@ -266,4 +261,166 @@ Proof.
   unfold lifted_plustwo.
   simpl. reflexivity.
 Qed.
+*)
 
+(* Automatic lifting *)
+Inductive ty' : Type :=
+  | Arrow' : ty' -> ty' -> ty'
+  | Nat' : ty'.
+
+Fixpoint lift_ty (T : ty) : ty' :=
+  match T with
+    | Nat => Nat'
+    | Arrow T1 T2 => Arrow' (lift_ty T1) (lift_ty T2)
+  end.
+
+Inductive tm' :=
+  | var' : string -> tm'
+  | abs' : string ->  ty' -> tm' -> tm'
+  | app' : tm' -> tm' -> tm'
+
+  | const' : (list (nat * PresenceCondition)) -> tm'
+  | succ' : tm' -> tm'.
+
+Fixpoint lift (t:tm) : tm':=
+  match t with
+  | var s => (var' s)
+  | abs s T t => (abs' s (lift_ty T) (lift t))
+  | STLC_SuccNat.app t1 t2 => (app' (lift t1) (lift t2))
+
+  | const n => (const' [(n, [[TRUE]])])
+  | succ t => (succ' (lift t))
+  end.
+
+Fixpoint derive_ty (T':ty') : ty :=
+  match T' with
+  | Nat' => Nat
+  | Arrow' T1' T2' => Arrow (derive_ty T1') (derive_ty T2')
+  end.
+
+Fixpoint derive_const_list (lst : list (nat * PresenceCondition)) (cfg : FeatureConfig) : option tm :=
+  match lst with
+  | [] => None
+  | (t, pc) :: rest =>
+    if pc_eval cfg pc then Some (const t)
+    else derive_const_list rest cfg
+  end.
+
+Fixpoint derive (t':tm') (cfg:FeatureConfig) : option tm :=
+  match t' with
+  | var' s => Some (var s)
+  | abs' s T' t1' =>
+    match derive t1' cfg with
+    | Some t1 => Some (abs s (derive_ty T') t1)
+    | None => None
+    end
+  | app' t1' t2' =>
+    match derive t1' cfg, derive t2' cfg with
+    | Some t1, Some t2 => Some (STLC_SuccNat.app t1 t2)
+    | _, _ => None
+    end
+  | const' n' => derive_const_list n' cfg
+  | succ' t1' =>
+    match derive t1' cfg with
+    | Some t1 => Some (succ t1)
+    | None => None
+    end
+  end.
+
+Example const1': lift (const 1) = const' [(1, [[TRUE]])].
+Proof. simpl. reflexivity. Qed.
+
+Example plusone': lift plusone = abs' "n" Nat' (succ' (var' "n")).
+Proof. simpl. reflexivity. Qed.
+
+Definition x':= const' [
+   (1, [[FEATURE "A"]]);
+   (2, [[~ (FEATURE "A") /\ (FEATURE "B")]]);
+   (3, [[~ (FEATURE "A") /\ ~ (FEATURE "B")]])
+  ].
+
+Definition y' := const' [
+    (5, [[(FEATURE "A") /\ ~ (FEATURE "B")]]);
+    (4, [[FEATURE "B"]]);
+    (3, [[~(FEATURE "A") /\ ~(FEATURE "B")]])
+  ].
+
+Definition z' := const' [ (19, [[TRUE]]) ].
+
+Definition disjoint (pc1 pc2 : PresenceCondition) : Prop :=
+  forall (cfg : FeatureConfig), ~(pc_eval cfg pc1 = true /\ pc_eval cfg pc2 = true).
+
+Fixpoint disjoint_lst (l : list(nat * PresenceCondition)) : Prop :=
+  match l with
+  | [] => True
+  | (_, pc) :: rest =>
+      (forall t' pc', In (t', pc') rest -> disjoint pc pc') /\ disjoint_lst rest
+  end.
+
+Fixpoint disjointness (t' : tm') : Prop :=
+  match t' with
+  | var' s => True
+  | abs' s T' t1' => disjointness t1'
+  | app' t1' t2' => disjointness t1' /\ disjointness t2'
+  | const' n' => disjoint_lst n'
+  | succ' t1' => disjointness t1'
+  end.
+
+Lemma disjointness_x': disjointness x'.
+Proof.
+  unfold disjointness.
+  simpl. split.
+  - intros t' pc' [].
+    + injection H as []. rewrite <- H.
+      unfold disjoint; intros cfg [].
+      simpl in H0, H1.
+      destruct in_dec; discriminate.
+    + destruct H.
+      * injection H as []. rewrite <- H.
+        intros cfg []. simpl in H0, H1.
+        destruct in_dec; discriminate.
+      * destruct H.
+  - split.
+    + intros t' pc' [].
+      * injection H as []. rewrite <- H.
+        intros cfg []. simpl in H0, H1.
+        destruct in_dec; destruct in_dec; discriminate.
+      * destruct H.
+    + split.
+      * intros. destruct H.
+      * apply I.
+Qed.
+
+Inductive value' : tm' -> Prop :=
+  | v_abs' : forall x T' t', value' (abs' x T' t')
+  | v_nat' : forall n', value' (const' n').
+
+Fixpoint subst' (x:string) (s' t': tm'): tm' :=
+  match t' with
+  | var' y => if String.eqb x y then s' else t'
+  | abs' y T' t1' => if String.eqb x y then t1' else abs' y T' (subst' x s' t1')
+  | app' t1' t2' => app' (subst' x s' t1') (subst' x s' t2')
+  | const' _ => t'
+  | succ' t1' => succ' (subst' x s' t1')
+  end.
+
+Inductive step': tm' -> tm' -> Prop :=
+  | ST_App1': forall t1' t1'' t2',
+    step' t1' t1'' ->
+      step' (app' t1' t2') (app' t1'' t2')
+  | ST_App2': forall v' t2' t2'',
+    value' v' ->
+    step' t2' t2'' ->
+      step' (app' v' t2') (app' v' t2'')
+  | ST_AppAbs': forall x T' t' v',
+    value' v' ->
+      step' (app' (abs' x T' t') v') (subst' x v' t')
+  | ST_Succ': forall t' t'',
+    step' t' t'' ->
+      step' (succ' t') (succ' t'')
+  | ST_SuccConst': forall n',
+      step' (succ' (const' n'))
+      (const' (map (fun '(n,pc) => ((S n), pc)) n')).
+
+Definition step'_normal_form_of t' t'':=
+  (multi step t' t'' /\ normal_form step t'').
