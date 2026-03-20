5f4980b8d74c6f3338bc85599f7d5090decf1f78
Arthur Diehl Barroso <arthurdiehlbarroso@gmail.com>
2026-03-20 16:09:12 -0300

    Added function for application of binary operators on variational values.

--- STLC_SuccNat/Presence_Conditions.v
+++ STLC_SuccNat/Presence_Conditions.v
@@ -42,3 +42,27 @@ Fixpoint derive {T} (v' : variational_value T) (cfg : feat_config) : option T :=
     if pc_eval cfg pc then Some v
     else derive rest cfg
   end.
+
+(* A binary operator like addition over variational natural values
+   would need to operate over all combinations of naturals and
+   presence conditions pairs. Much like vector product.
+   For example:
+      [(1,A);(0,~A)] + [(2,B);(3,~B)]
+  ==> [(1+2,A/\B);(1+3,A/\~B);(0+2,~A/\B);(0+3,~A/\~B)]
+  ==> [(3,A/\B);(4,A/\~B);(2,~A/\B);(3,~A/\~B)]*)
+
+Compute map (fun n => n +2) [1; 2; 3].
+Compute map (fun '(v2, pc2) => ((2 + v2) , (pc_And pc_True pc2))) [(1, pc_True); (2,pc_False)].
+
+Fixpoint app_binop {T} (op : T -> T -> T)
+  (v1' : variational_value T) (v2' : variational_value T) : (variational_value T) :=
+  match v1' with
+  | [] => []
+  | (v1, pc1) :: rest => (map (fun '(v2, pc2) => ((op v1 v2), (pc_And pc1 pc2))) v2') ++
+                          (app_binop op rest v2')
+  end.
+
+Compute app_binop (fun n1 n2 => n1 + n2) [(1,pc_True);(0,pc_False)] [(2,pc_True);(3,pc_False)].
+
+
+
