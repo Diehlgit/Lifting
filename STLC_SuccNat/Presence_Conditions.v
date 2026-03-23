6ce45f652e0e34940eb96ec5dda2b1fe18458a68
Arthur Diehl Barroso <arthurdiehlbarroso@gmail.com>
2026-03-23 16:40:46 -0300

    Added definition of application of binary operators on variational values and related lemmas.

--- STLC_SuccNat/Presence_Conditions.v
+++ STLC_SuccNat/Presence_Conditions.v
@@ -51,18 +51,71 @@ Fixpoint derive {T} (v' : variational_value T) (cfg : feat_config) : option T :=
   ==> [(1+2,A/\B);(1+3,A/\~B);(0+2,~A/\B);(0+3,~A/\~B)]
   ==> [(3,A/\B);(4,A/\~B);(2,~A/\B);(3,~A/\~B)]*)
 
-Compute map (fun n => n +2) [1; 2; 3].
-Compute map (fun '(v2, pc2) => ((2 + v2) , (pc_And pc_True pc2))) [(1, pc_True); (2,pc_False)].
-
 Fixpoint app_binop {T} (op : T -> T -> T)
   (v1' : variational_value T) (v2' : variational_value T) : (variational_value T) :=
   match v1' with
   | [] => []
   | (v1, pc1) :: rest => (map (fun '(v2, pc2) => ((op v1 v2), (pc_And pc1 pc2))) v2') ++
-                          (app_binop op rest v2')
+                         (app_binop op rest v2')
   end.
 
-Compute app_binop (fun n1 n2 => n1 + n2) [(1,pc_True);(0,pc_False)] [(2,pc_True);(3,pc_False)].
+Compute app_binop Nat.add [(1,pc_True);(0,pc_False)]
+                          [(2,pc_True);(3,pc_False)].
 
+Compute app_binop Nat.add [(1,pc_True);(0,pc_False)] [].
 
+Compute app_binop Nat.add [] [(2,pc_True);(3,pc_False)].
 
+Lemma app_binop_assoc {T} : forall (op:T->T->T) (v1' v2':variational_value T) (v:T) (p:pc),
+  app_binop op ((v, p) :: v1') v2' =
+  (app_binop op [(v, p)] v2') ++ (app_binop op v1' v2').
+Proof.
+  intros. destruct v1'.
+  - simpl. f_equal.
+    rewrite app_nil_r.
+    reflexivity.
+  - destruct p0; simpl.
+    f_equal. rewrite app_nil_r.
+    reflexivity.
+Qed.
+
+Lemma derive_l {T} : forall (conf:feat_config) (v1' v2':variational_value T) (v:T),
+  derive v1' conf = Some v ->
+  derive (v1' ++ v2') conf = Some v.
+Proof.
+  intros. induction v1'; simpl.
+  - inversion H.
+  - destruct a, (pc_eval conf p) eqn:Eq;
+      simpl in H; rewrite Eq in H.
+    + assumption.
+    + apply IHv1', H.
+Qed.
+
+Lemma derive_binop_none {T} : forall (conf:feat_config) (op:T->T->T)
+                              (v':variational_value T) (n:T) (p:pc),
+  pc_eval conf p = false ->
+  derive (app_binop op [(n, p)] v') conf = None.
+Proof.
+  intros. simpl.
+  rewrite app_nil_r.
+  induction v'.
+  - reflexivity.
+  - destruct a. simpl.
+    rewrite H. simpl.
+    auto.
+Qed.
+
+Lemma derive_r {T} : forall (conf:feat_config) (v1' v2':variational_value T) (r:option T),
+  derive v1' conf = None ->
+  derive v2' conf = r ->
+  derive (v1' ++ v2') conf = r.
+Proof.
+  intros. induction v1'; simpl.
+  - auto.
+  - destruct a; simpl.
+    simpl in H.
+    destruct (pc_eval conf p).
+    inversion H.
+    apply IHv1'.
+    assumption.
+Qed.
